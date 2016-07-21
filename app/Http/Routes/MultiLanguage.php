f31fa03dc65212421f15b1b0c371a9b5a0d7cd5c
Matt Ma <395241021@qq.com>
2016-07-21 10:48:02 +0800

    多语言支持

--- app/Http/Routes/MultiLanguage.php
+++ app/Http/Routes/MultiLanguage.php
@@ -1,8 +1,9 @@
 <?php
 
-$languages = Request()->getLanguages();
+$languages     = Request()->getLanguages();
+$yamlLanguages = languages();
 
-if ( Request()->segment( 1 ) != 'localization' ) {
+if ( ( Request()->segment( 3 ) != 'localization' ) && !is_null( $yamlLanguages ) ) {
     if ( is_array( $languages ) ) {
         $language = str_replace( '_', '-', strtolower( $languages[ 0 ] ) );
 
@@ -11,7 +12,7 @@ if ( Request()->segment( 1 ) != 'localization' ) {
         if ( $cookieLanguage && in_array( $cookieLanguage, array_keys( languages() ) ) ) {
             app()->setLocale( $cookieLanguage );
         } else {
-            app()->setLocale( \App\Models\Configure::whereKey('web_lang')->first()->value );
+            app()->setLocale( \App\Models\Configure::whereKey( 'web_lang' )->first()->value );
             Cookie()->queue( Cookie()->forever( 'language', $language ) );
         }
     }
---
+++ app/Plugins/Localization/Localization.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Plugins\Localization;
+
+use Illuminate\Http\Request;
+
+class Localization
+{
+    public function run()
+    {
+        $type = Request()->segment( 4 );
+
+        switch ( $type ) {
+            case 'zh-cn':
+                return $this->setLocalization( 'zh-cn' );
+            case 'zh-tw':
+                return $this->setLocalization( 'zh-tw' );
+        }
+
+        abort( 404 );
+    }
+
+    protected function setLocalization( $language )
+    {
+        app()->setLocale( $language );
+        Cookie()->queue( Cookie()->forever( 'language', $language ) );
+
+        return Redirect::back();
+    }
+}
